cea071ec859cfe6eeab8ea08b4c5cafe9afc9fe3
raphavs <raphavosseler@googlemail.com>
2024-04-17 14:27:31 +0200

    adding metadata

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,17 +6,21 @@ import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
- 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
+
 export default async function Page({
-  searchParams
+  searchParams,
 }: {
   searchParams?: {
-    query?: string,
-    page?: string
+    query?: string;
+    page?: string;
   };
 }) {
-
-  const query = searchParams?.query || '';
+  const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
 
   const totalPages = await fetchInvoicesPages(query);
